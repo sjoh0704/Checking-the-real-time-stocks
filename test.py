5530502b5600d7ca67b95e507de13e8c3c81c035
SeungJu <sjoh0704@daum.net>
2021-02-21 19:41:52 +0900

    add closed error exception

--- test.py
+++ test.py
@@ -4,14 +4,17 @@ import sys
 import pygame
 import threading
 
-WIDTH = 450
-HEIGHT = 250
+X = 30
+WIDTH = 400
+height = 400
 TEXT_SIZE = 25
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+WHITE = (246, 246, 246)
+BLACK = (25, 25, 25)
 SPACE = 30
+BLUE = (0, 84, 255)
+RED = (255, 0, 0)
 
-link_list = ["https://finance.yahoo.com/quote/UAVS?p=UAVS&.tsrc=fin-srch", "https://finance.yahoo.com/quote/ATNF?p=ATNF&.tsrc=fin-srch"]
+link_list = list()
 my_list = []
 class Item:
     def __init__(self,title, main, sub, main_add, sub_add):
@@ -68,6 +71,7 @@ def runPad():
     txt = "waiting"
     exit = False
     tmp = 1
+    line = "_" * 100
 
     font = pygame.font.SysFont(None, TEXT_SIZE)
     pygame.display.update()
@@ -82,19 +86,20 @@ def runPad():
                 pygame.quit()
                 sys.exit()
 
-        pad.fill(WHITE)
+        pad.fill(BLACK)
 
 
-        print(threading.active_count())
         if threading.active_count() == 1:
             for i in range(len(link_list)):
+
                 thread = threading.Thread(target=business, args=(link_list[i], my_list))
                 thread.start()
 
+
         if len(before) == 0:
 
-            tmp_text = font.render(txt, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-            pad.blit(tmp_text, (WIDTH // 2 - 100, HEIGHT // 2))
+            tmp_text = font.render(txt, True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
+            pad.blit(tmp_text, (WIDTH // 2 - 100, height // 2))
             txt += "."
             if txt == "waiting.......":
                 txt = "waiting"
@@ -103,46 +108,151 @@ def runPad():
 
         if len(my_list) == len(link_list):
             before = list(my_list)
+
             for i in range(len(my_list)):
                 elem = my_list.pop(0)
+
+
                 y += SPACE
-                text1 = font.render(elem.title, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-                pad.blit(text1, (50, y))
+                text1 = font.render(elem.title, True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
+                pad.blit(text1, (X, y))
                 y += SPACE
-                text2 = font.render(elem.main + ' ' + elem.main_add, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-                pad.blit(text2, (50, y))
+
+                if (elem.main_add == 0 and elem.main == 0) or (elem.main_add == "" and elem.main == ""):
+                    text2 = font.render("closed", True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
+
+                elif elem.main_add[0] == "+":
+                    text2 = font.render(str(elem.main) + ' ' + str(elem.main_add), True, BLUE)  # 텍스트가 표시된 Surface 를 만듬
+                else:
+                    text2 = font.render(str(elem.main) + ' ' + str(elem.main_add), True, RED)
+                pad.blit(text2, (X, y))
                 y += SPACE
-                text3 = font.render(elem.sub + ' ' + elem.sub_add, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-                pad.blit(text3, (50, y))
+
+                if (elem.sub_add == 0 and elem.sub == 0) or (elem.sub_add == "" and elem.sub == ""):
+                    text3 = font.render("closed", True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
+
+                elif elem.sub_add[0] == "+":
+                    text3 = font.render(str(elem.sub) + ' ' + str(elem.sub_add), True,
+                                        BLUE)  # 텍스트가 표시된 Surface 를 만듬
+
+                else:
+                    text3 = font.render(str(elem.sub) + ' ' + str(elem.sub_add), True, RED)
+                pad.blit(text3, (X, y))
+
+                y += 10
+
+
+                text4 = font.render(line, True, WHITE)
+                pad.blit(text4, (0, y))
+
+
+
+
+
 
         else:
+
             for i in range(len(before)):
                 y += SPACE
-                text1 = font.render(before[i].title, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-                pad.blit(text1, (50, y))
+                text1 = font.render(str(before[i].title), True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
+                pad.blit(text1, (X, y))
                 y += SPACE
-                text2 = font.render(before[i].main + ' ' + before[i].main_add, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-                pad.blit(text2, (50, y))
+
+                if (before[i].main_add == 0 and before[i].main == 0) or (before[i].main_add == "" and before[i].main == ""):
+                    text2 = font.render("closed", True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
+
+                elif before[i].main_add[0] == "+":
+                    text2 = font.render(str(before[i].main) + ' ' + str(before[i].main_add), True, BLUE)  # 텍스트가 표시된 Surface 를 만듬
+                else:
+                    text2 = font.render(str(before[i].main) + ' ' + str(before[i].main_add), True, RED)
+
+                pad.blit(text2, (X, y))
                 y += SPACE
-                text3 = font.render(before[i].sub + ' ' + before[i].sub_add, True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
-                pad.blit(text3, (50, y))
+                if (before[i].sub_add == 0 and before[i].sub == 0) or (before[i].sub_add == "" and before[i].sub == ""):
+
+                    text3 = font.render("closed", True,
+                                        WHITE)  # 텍스트가 표시된 Surface 를 만듬
+
+                elif before[i].sub_add[0] == "+":
+                    text3 = font.render(str(before[i].sub) + ' ' + str(before[i].sub_add), True,
+                                        BLUE)  # 텍스트가 표시된 Surface 를 만듬
+                else:
+                    text3 = font.render(str(before[i].sub) + ' ' + str(before[i].sub_add), True, RED)
+                pad.blit(text3, (X, y))
+
+                y += 10
+
+                text4 = font.render(line, True, WHITE)
+                pad.blit(text4, (0, y))
 
 
-        tmp_text = font.render(str(tmp), True, BLACK)  # 텍스트가 표시된 Surface 를 만듬
+        tmp_text = font.render(str(tmp), True, WHITE)  # 텍스트가 표시된 Surface 를 만듬
         tmp += 1
-        pad.blit(tmp_text, (WIDTH-50,HEIGHT-50))
+        pad.blit(tmp_text, (WIDTH-2*X,height-X))
         pygame.display.update()
         clk.tick(5)
 
 
 
 def initPad():
+    global height
     global pad, clk
     pygame.init()
-    pad = pygame.display.set_mode((WIDTH, HEIGHT))
+    height = 130 * len(link_list)
+    pad = pygame.display.set_mode((WIDTH, height))
     pygame.display.set_caption("Real-time STOCKS")
     clk = pygame.time.Clock()
     runPad()
+
+def search():
+
+    going = True
+    while going:
+        search = input("추가하고 싶은 항목 검색: ")
+        link = "https://finance.yahoo.com/quote/" + search
+        response = requests.get(link)
+
+        if response.status_code == 200:
+            html = response.text
+            soup = BeautifulSoup(html, "html.parser")
+            # titles = soup.select('ul.list_basis div div div div div a[title]')
+            try:
+                print("...\n잠시만 기다려 주세요...\n...")
+                title = soup.select_one('div div div div div div div div div div h1').get_text()
+                print(title)
+            except:
+                print("잘못된 페이지 입니다.")
+
+            else:
+                print("링크를 추가합니다.")
+                link_list.append(link)
+
+            finally:
+
+                ans = input("\n계속 추가하시겠습니까?(y/n): ")
+                if ans == "y" or ans == "Y":
+                    continue
+                elif ans == "n" or ans == "N":
+                    print("종료합니다.")
+                    going = False
+
+                else:
+                    print("계속 추가합니다.")
+                    continue
+
+
+
+
+        else:
+
+            print("ERROR: "+ str(response.status_code))
+            print("검색을 재시도 합니다.")
+
+
+
+
+search()
+
 initPad()
 
 
